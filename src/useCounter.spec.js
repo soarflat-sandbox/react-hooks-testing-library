d62b4f4ad9376e7cf3dd5ef9e97e59ebe9424b18
soarflat <soarflat777@gmail.com>
2021-03-22 16:40:16 +0900

    test: Basic Hooks

--- src/useCounter.spec.js
+++ src/useCounter.spec.js
@@ -3,8 +3,14 @@ import { renderHook, act } from '@testing-library/react-hooks';
 import useCounter from './useCounter';
 
 test('should use counter', () => {
+  // renderHook を実行すると、フックをテストするためのコンポーネントが生成される
+  // テストコンポーネントがレンダリングされるたびに、第一引数のコールバック関数が呼び出される。
+  // コールバック関数内では、必ずテスト用のフックを１つ以上呼び出す必要がある。
+  // 今回は useCounter を呼び出している。
   const { result } = renderHook(() => useCounter());
 
+  // result.current には、呼び出したフックの戻り値が格納されるため、
+  // それを利用してアサーションする。
   expect(result.current.count).toBe(0);
   expect(typeof result.current.increment).toBe('function');
 });
@@ -12,6 +18,7 @@ test('should use counter', () => {
 test('should increment counter', () => {
   const { result } = renderHook(() => useCounter());
 
+  // テストコンポーネントを更新するロジックは act でラップする。
   act(() => {
     result.current.increment();
   });
---
+++ src/useCounterWithProps.spec.js
@@ -0,0 +1,32 @@
+import { renderHook, act } from '@testing-library/react-hooks';
+
+import useCounterWithProps from './useCounterWithProps';
+
+test('should increment counter from custom initial value', () => {
+  const { result } = renderHook(() => useCounterWithProps(9000));
+
+  act(() => {
+    result.current.increment();
+  });
+
+  expect(result.current.count).toBe(9001);
+});
+
+test('should reset counter to updated initial value', () => {
+  // 第１引数のコールバック関数に渡される引数は、第２引数の initialProps に指定した値になる。
+  // そのため、今回は initialValue に 0 が渡される。
+  const { result, rerender } = renderHook(({ initialValue }) => useCounterWithProps(initialValue), {
+    initialProps: { initialValue: 0 },
+  });
+  expect(result.current.count).toBe(0);
+
+  // テストコンポーネントを再レンダリングする。
+  // 上記のコールバック関数の引数 initialValue に 10 が渡されて、テストコンポーネントが再レンダリングされる。
+  rerender({ initialValue: 10 });
+
+  act(() => {
+    result.current.reset();
+  });
+
+  expect(result.current.count).toBe(10);
+});
